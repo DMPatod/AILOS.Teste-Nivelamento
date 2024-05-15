5c50d1587808e88b4aa840a7a1691d2deac061d5
DMPatod <danielmaiaevangelista@gmail.com>
2024-05-14 21:47:14 -0300

    Questao 1

--- Questao1/ContaBancaria.cs
+++ Questao1/ContaBancaria.cs
@@ -1,9 +1,36 @@
-﻿using System.Globalization;
-
-namespace Questao1
+﻿namespace Questao1
 {
-    class ContaBancaria {
+    class ContaBancaria
+    {
+        public ContaBancaria(int numeroConta, string titular)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+        }
+        public ContaBancaria(int numeroConta, string titular, double deposito)
+        {
+            NumeroConta = numeroConta;
+            Titular = titular;
+            Deposito(deposito);
+        }
+        public int NumeroConta { get; }
+        public string Titular { get; set; }
+        public double Saldo { get; private set; } = 0;
+        public void Deposito(double valor)
+        {
+            Saldo += valor;
+        }
+        public void Saque(double valor)
+        {
+            Saldo -= (valor + TaxaSaque);
+        }
 
-       
+        private readonly double TaxaSaque = 3.5;
+
+        public override string ToString()
+        {
+            return $"Conta {NumeroConta}, Titular: {Titular}, Saldo: $ {Saldo:F2}";
+        }
+
     }
 }
